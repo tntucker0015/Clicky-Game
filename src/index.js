0e9e99d03a5340a789c66c95a501edf6262f24ca
Tim Tucker <tntucker0015@outlook.com>
2018-07-13 16:36:39 -0400

    adding images

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+//renders the App.js file to the root element (index.html in public folder)
+ReactDOM.render(<App />, document.getElementById("root"));
